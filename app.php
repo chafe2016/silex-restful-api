6765d582d3607c108f38cba8e6a0fb14029a9ffd
Rodrigo Muniz <muniz95@bsd.com.br>
2016-06-01 18:48:21 +0000

    Função para obter parte do CSV de dados do 156

--- app.php
+++ app.php
@@ -2,6 +2,37 @@
 
 require_once __DIR__.'/vendor/autoload.php'; 
 
+function csvToArray($file, $delimiter) { 
+    if (($handle = getFilePart(fopen($file, 'r'), 20)) !== FALSE) { 
+        $i = 0; 
+        while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) {
+            for ($j = 0; $j < count($lineArray); $j++) {
+                $arr[$i][$j] = $lineArray[$j];
+            } 
+            $i++; 
+        } 
+        fclose($handle); 
+    } 
+    return $arr;
+}
+
+function getFilePart($file, $lines){
+    $new_file = tmpfile();
+    if($file){
+        $i = 0;
+        while ($i <= $lines) {
+            fwrite($new_file, fgets($file));
+            $i++;
+        }
+        // fclose($new_file);
+    } 
+    else {
+        return false;
+    }
+    fseek($new_file, 0);
+    return $new_file;
+}
+
 $app = new Silex\Application(); 
 
 $app->get('/', function() use($app) { 
@@ -10,6 +41,21 @@ $app->get('/', function() use($app) {
 
 $app->get('/hello/{name}', function($name) use($app) { 
     return 'Hello '.$app->escape($name); 
+}); 
+
+$app->get('/api/dados156', function() use($app) {
+    $json = csvToArray(getcwd() . "/data/156_-_Base_de_Dados.csv", ";");
+    // $handle = getFilePart(fopen(getcwd() . "/data/156_-_Base_de_Dados.csv", 'r'), 20);
+    // if ($handle) {
+    //     while (($line = fgets($handle)) !== false) {
+    //         print_r($line);
+    //     }
+    //     fclose($handle);
+    // } else {
+    //     return false;
+    // }
+    print_r($json);
+    return true;
 }); 
 
 $app->run(); 
