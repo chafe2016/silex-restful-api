0aacd0f183771ee531931a6268855a0e772fdea9
Rodrigo Muniz <muniz95@bsd.com.br>
2016-06-01 20:18:26 +0000

    Formatação de CSV para JSON concluída

--- app.php
+++ app.php
@@ -16,24 +16,43 @@ function csvToArray($file, $delimiter) {
     return $arr;
 }
 
+function arrayToJson($array){
+    $json_out = []; // Saída em JSON
+    $document = []; // Cada elemento do JSON final
+    $json_indexes = ["SOLICITACAO","TIPO","ORGAO","DATA","HORARIO","ASSUNTO","SUBDIVISAO","DESCRICAO","LOGRADOURO_ASS",
+        "BAIRRO_ASS","REGIONAL_ASS","MEIO_RESPOSTA","OBSERVACAO","SEXO","BAIRRO_CIDADAO","REGIONAL_CIDADAO","DATA_NASC",
+        "TIPO_CIDADAO","ORGAO_RESP","RESPOSTA_FINAL"]; // Conjunto de chaves de cada documento do JSON
+    foreach ($array as $line) {
+        foreach ($line as $index => $cell) {
+            $document[$json_indexes[$index]] = $cell; // Adiciona a célula do CSV à sua respectiva chave no documento
+        }
+        array_push($json_out, $document); // Adiciona o documento ao JSON
+    }
+    
+    return $json_out;
+}
+
 function getFilePart($file, $lines){
-    $new_file = tmpfile();
+    $new_file = tmpfile(); 
     if($file){
         $i = 0;
         while ($i <= $lines) {
-            fwrite($new_file, fgets($file));
+            $line = utf8_encode(fgets($file)); // Pega linha por linha do CSV
+            if($i >= 2) // Ignora as 2 primeiras linhas do CSV (título e uma outra linha de lixo)
+                fwrite($new_file, $line); // Escreve a linha para o arquivo temporário
             $i++;
         }
-        // fclose($new_file);
     } 
     else {
         return false;
     }
-    fseek($new_file, 0);
+    fseek($new_file, 0); // Necessário pro arquivo poder ser exportado
     return $new_file;
 }
 
 $app = new Silex\Application(); 
+
+$app['debug'] = true;
 
 $app->get('/', function() use($app) { 
     return 'Hello World'; 
@@ -44,18 +63,8 @@ $app->get('/hello/{name}', function($name) use($app) {
 }); 
 
 $app->get('/api/dados156', function() use($app) {
-    $json = csvToArray(getcwd() . "/data/156_-_Base_de_Dados.csv", ";");
-    // $handle = getFilePart(fopen(getcwd() . "/data/156_-_Base_de_Dados.csv", 'r'), 20);
-    // if ($handle) {
-    //     while (($line = fgets($handle)) !== false) {
-    //         print_r($line);
-    //     }
-    //     fclose($handle);
-    // } else {
-    //     return false;
-    // }
-    print_r($json);
-    return true;
+    $json = csvToArray(getcwd() . "/data/dados156_simples.csv", ";");
+    return $app->json(arrayToJson($json));
 }); 
 
 $app->run(); 
